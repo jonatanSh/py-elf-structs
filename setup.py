de5c1397f306c62f280f8773e7f49640ddaf2e95
jonatansh <jonatanshimon@gmail.com>
2022-04-22 21:09:46 +0300

    convert readme to rst

--- setup.py
+++ setup.py
@@ -5,18 +5,19 @@ try:
     import pypandoc
 
     README = pypandoc.convert(os.path.join(os.path.dirname(__file__), 'README.md'), 'rst')
-except (ImportError, OSError):
-    print("Can't convert readme")
+except (ImportError, OSError) as e:
+    print("Can't convert readme: {}".format(e))
     README = ""
 
 setup(
     name='py_elf_structs',
-    version='1',
+    version='1.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['cstruct==1.8', 'pyelftools'],
     license='MIT License',
-    description='A simple Django app to create rest applications',
+    description='Python package to extract struct and type information from dwarf and build python cstructs',
+    long_description=README,
     url='https://github.com/jonatanSh/py-elf-structs',
     author='Jonathan Shimon',
     author_email='jonatanshimon@gmail.com',
